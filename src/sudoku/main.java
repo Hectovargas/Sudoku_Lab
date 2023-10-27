8dc38d7c21ea4e1458f9d45942a6d6a9d1ca0372
Hector <Hector@fefo>
2023-10-27 14:27:48 -0600

    Sudoku

---
+++ src/sudoku/main.java
@@ -0,0 +1,11 @@
+
+package sudoku;
+
+public class main {
+   
+    public static void main(String[] args) {
+         Tablero juego = new Tablero();
+         
+         juego.setVisible(true);
+    }
+}
